1a925259886d9bd2656645899dac69a34d31cf80
masud001 <madhnagar@gmail.com>
2021-07-12 01:18:21 +0600

    Hello Python orld

---
+++ hello.py
@@ -0,0 +1,13 @@
+"""
+this is the multiline  comment
+this is the multiline  comment
+this is the multiline  comment
+this is the multiline  comment
+
+"""
+
+# Python ADD two number
+x = input('input num1 : ')
+y = input('input num1 : ')
+sum = float(x) + float(y)
+print('the sum on {} and {} is = {}'.format(x, y, sum))
